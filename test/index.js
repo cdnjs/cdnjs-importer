4f330d544594bbe336f8ab03e37d03eb0061a70b
Ionică Bizău <bizauionica@yahoo.com>
2015-03-22 14:25:51 +0200

    Removed debugger.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,9 @@
+// Dependencies
 var CdnJsAdder = require("../lib")
   , Path = require("path")
   ;
 
+// Test adding CaiuSS
 CdnJsAdder({
     cdnjs: Path.resolve(__dirname, "../../cdnjs")
   , debug: true
@@ -9,7 +11,5 @@ CdnJsAdder({
         "git@github.com:IonicaBizau/CaiuSS.git"
     ]
 }, function (res) {
-    debugger
+    console.log(res);
 });
-
-
